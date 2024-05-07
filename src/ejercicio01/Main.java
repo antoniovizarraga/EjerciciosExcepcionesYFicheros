bbaff91b3b3dbc91ab58fba8a4078340a135b7bc
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-05-07 14:36:50 +0200

    Ejercicio 1 comenzado

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,10 +1,21 @@
 package ejercicio01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un número entero: ");
+		int test = Utils.readInt(sc);
+		
+		System.out.println("Introduce un número con decimales: ");
+		double test2 = Utils.readDouble(sc);
+		
+		
+		sc.close();
 	}
 
 }
--- src/ejercicio01/Utils.java
+++ src/ejercicio01/Utils.java
@@ -1,5 +1,35 @@
 package ejercicio01;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Utils {
+	
+	public static int readInt(Scanner sc) {
+		int res = 0;
+		
+		try {
+			res = sc.nextInt();
+			sc.next();
+		} catch(InputMismatchException e) {
+			sc.next();
+			
+			System.out.println("El valor introducido no es de tipo int");
+		}
+		
+		return res;
+	}
+	
+	public static double readDouble(Scanner sc) {
+		double res = 0.0;
+		
+		try {
+			
+		} catch(InputMismatchException e) {
+			
+		}
+		
+		return res;
+	}
 
 }
